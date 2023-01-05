09a294db645b91492e77fb8aa9d24967611aa9ba
AghaAmil <moravveji.amir.92@gmail.com>
2023-01-05 22:55:34 +0330

    javascript overview

--- practive-js/1-overview.js
+++ practive-js/1-overview.js
@@ -1,3 +1,49 @@
+// first lesson
 console.log("1. First Lesson");
 console.log("Fresh start to learn JavaScript");
 
+// output
+document.getElementById("p1").innerHTML =
+  "This paragraph supposed to be generated with document.write() property. But to make things beautiful and some technical problems, I've produced it using JS DOM.";
+
+// variables
+console.log("\n");
+console.log("3. Variables");
+
+var x = 100;
+console.log("X = " + x);
+
+console.log("The value of x can be changed through your code");
+x = 101;
+console.log("X = " + x);
+
+// multiline comments
+/* var x = 100;
+console.log("X = " + x);
+
+console.log("The value of x can be changed through your code");
+x = 101;
+console.log("X = " + x); */
+
+// date types
+console.log("\n");
+console.log("5. Variables");
+
+console.log("X can be another number again");
+x = 34.1;
+console.log("X = " + x);
+
+var str = "This is an string";
+console.log(str);
+
+var str1 = "This is an another string iside a 'quotation'";
+console.log(str1);
+
+var str2 = 'This is an another string iside a "quotation"';
+console.log(str2);
+
+var bool1 = true;
+var bool2 = false;
+
+console.log(bool1);
+console.log(bool2);
