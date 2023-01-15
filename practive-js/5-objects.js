2bd3f337996e520d7f98d0c92ab4ff843fb2fc95
AghaAmil <moravveji.amir.92@gmail.com>
2023-01-15 23:48:58 +0330

    object constructor function

--- practive-js/5-objects.js
+++ practive-js/5-objects.js
@@ -24,3 +24,34 @@ var cuboid = {
 
 var vol = cuboid.length * cuboid.height * cuboid.width;
 document.getElementById("sp1").innerHTML = vol;
+
+// object constructor
+console.log("\n");
+console.log("2. Object Constructor Fucntion");
+
+function studentProfile(name, surname, id, major) {
+  this.fname = name;
+  this.lname = surname;
+  this.studentId = id;
+  this.major = major;
+}
+
+var student1 = new studentProfile("Amirhossein", "Moravveji", 7868, "Computer Engineering");
+console.log(student1);
+
+var student2 = new studentProfile("Ali", "Behnam", 3345, "Civil Engineering");
+console.log(student2);
+
+console.log("Student1 ID is: " + student1.studentId);
+console.log("Student1 Major is: " + student1["major"]);
+console.log("Student2 complete name is: " + student2.fname + " " + student2["lname"]);
+
+// Flight managment object
+function flightScheduler(flightNumber, flightStatus) {
+  this.flightNumber = flightNumber;
+  this.flightStatus = flightStatus;
+}
+
+var flight1 = new flightScheduler("NGT 929", "landed");
+document.getElementById("sp2").innerHTML =
+  "Flight " + flight1.flightNumber + " has " + flight1.flightStatus;
