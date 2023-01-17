0bc6a7dd8bb88e377d501faa55d3086420dcbdb9
AghaAmil <moravveji.amir.92@gmail.com>
2023-01-17 23:01:00 +0330

    arrays intro

---
+++ practive-js/6-core-objects.js
@@ -0,0 +1,7 @@
+// arrays.
+var breakfast = new Array("Cinnamon Doughnuts", "Waffles", "Granola", "Chorizo Burrito", "French Toast");
+console.log(breakfast);
+
+breakfast[2] = "Fluffy Pancakes";
+console.log("The index 2 is entered and the breakfast item is going to be changed");
+console.log(breakfast);
