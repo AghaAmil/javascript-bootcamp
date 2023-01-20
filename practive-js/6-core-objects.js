1c9145e1fdc842adc8ef154c83e9eaea30f3747b
AghaAmil <moravveji.amir.92@gmail.com>
2023-01-20 22:58:29 +0330

    other ways to create arrays

--- practive-js/6-core-objects.js
+++ practive-js/6-core-objects.js
@@ -5,3 +5,30 @@ console.log(breakfast);
 breakfast[2] = "Fluffy Pancakes";
 console.log("The index 2 is entered and the breakfast item is going to be changed");
 console.log(breakfast);
+
+// making arrays
+var courses = new Array(3);
+courses[0] = "HTML";
+courses[1] = "CSS";
+courses[2] = "JavaScript";
+
+console.log(courses);
+
+var course1 = new Array();
+course1[0] = "HTML";
+course1[1] = "CSS";
+course1[2] = "JS";
+course1[3] = "Angular";
+course1[4] = "React";
+
+console.log(course1);
+
+// array literals
+var course2 = ["Math", "Science", "Geometry", "English", "Algebra"];
+
+console.log(course2);
+console.log(course2[3]);
+
+course2[3] = "Arabic Studies";
+
+console.log(course2);
