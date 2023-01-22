26b421e7a248ae994d39fba91aedd8fb67e19c90
AghaAmil <moravveji.amir.92@gmail.com>
2023-01-22 23:28:46 +0330

    js date object and method

--- practive-js/6-core-objects.js
+++ practive-js/6-core-objects.js
@@ -67,3 +67,55 @@ document.getElementById("sp1").innerHTML = Math.ceil(year / 100);
 // var answer = Math.sqrt(input);
 
 // alert("The square root of " + input + " is : " + answer.toFixed(2));
+
+// setInternal
+var minInSeconds = 60;
+
+function counter() {
+  minInSeconds -= 1;
+  document.getElementById("sp2").innerHTML = minInSeconds;
+
+  if (minInSeconds == 0) {
+    clearInterval(timer1);
+    document.getElementById("sp2").innerHTML = "Time's Up!";
+  }
+}
+
+var timer1 = setInterval(counter, 1000);
+
+// date()
+document.getElementById("sp3").innerHTML = new Date();
+document.getElementById("sp4").innerHTML = new Date(86400000);
+document.getElementById("sp5").innerHTML = new Date("January 2, 2015 10:42:00");
+document.getElementById("sp6").innerHTML = new Date(88, 5, 11, 11, 42, 0, 0);
+
+// date() methods
+console.log("\n");
+console.log("Date object & methods...");
+
+console.log("Current time (Only fullyear is displayed): " + new Date().getFullYear());
+console.log("Current time (Only month is displayed): " + new Date().getMonth());
+console.log("Current time (Only hour is displayed): " + new Date().getHours());
+console.log("Current time (Only mintue is displayed): " + new Date().getMinutes());
+
+// digital clock
+
+function digitalClock() {
+  var d = new Date();
+  var hour = d.getHours();
+  var min = d.getMinutes();
+  var sec = d.getSeconds();
+  document.getElementById("sp7").innerHTML = hour + ":" + min + ":" + sec;
+}
+
+var timer2 = setInterval(digitalClock, 1000);
+
+// example
+function getWeekDay(year, month, day) {
+  var names = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  var d1 = new Date(year, month, day);
+
+  return names[d1.getDay()];
+}
+
+document.getElementById("sp8").innerHTML = getWeekDay(1993, 7, 24);
