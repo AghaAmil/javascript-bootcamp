c2bff9e5e8ba47db133de525be79f4a62639260f
Amirhossein Moravveji <54990581+AghaAmil@users.noreply.github.com>
2023-07-18 19:57:18 +0330

    correction on old files + the if statement

---
+++ practice-files/lesson3/jsFile.js
@@ -0,0 +1,13 @@
+console.log('1. The if Statement');
+
+let output = 'September'
+if (output === 'August') {
+    console.log('Vacation');
+}
+
+let output2 = 'August'
+if (output2 === 'August') {
+    console.log('Vacation');
+}
+
+console.log('\n');
