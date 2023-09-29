5464209796ce1665f9e75748831dd4f345f5ca02
Amirhossein Moravveji <54990581+AghaAmil@users.noreply.github.com>
2023-09-29 23:10:01 +0330

    lesson one - js introduction is completed

--- sololearn-courses/js/lesson1.js
+++ sololearn-courses/js/lesson1.js
@@ -1,2 +1,12 @@
 console.log('The Console - The Output');
 console.log('Hello World! as usual :)');
+console.log('All good, no errors');
+console.log('Hello Again!');
+
+console.log('I\'m practicing JavaScript!. This is an statement.');
+
+// generate a blank line.
+console.log('\n');
+
+console.log('Exercise 1: My First Code Coach!')
+console.log('Solving my first JavaScript Code Coach!');
